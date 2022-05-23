c3c58ca6dbef98ce2447d75598a8c42ab206bc22
Tom Groombridge <tom.groombridge@zopa.com>
2022-05-23 16:32:31 +0200

    remove create code

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,57 +1,7 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-const Create = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
-  let navigate = useNavigate();
-
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-    const data = {title, body, author}
-    console.log(data);
-    try{
-      await fetch('http://localhost:3001/blogs', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      navigate('/')
-    } catch (error) {
-      
-    }
-  }
-
+const Create = () => {  
   return (
     <div className="create">
-      <h2>Add a New Blog</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Blog title:</label>
-        <input 
-          type="text" 
-          required 
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label>Blog body:</label>
-        <textarea
-          required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-        <label>Blog author:</label>
-        <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
-        <button>Add Blog</button>
-      </form>
+      <h2>Add a New Blog</h2>      
     </div>
   );
 }
